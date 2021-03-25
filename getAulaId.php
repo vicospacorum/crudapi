69f43b678f74861623578c8dbf647b0a6b1e9e4d
vicospacorum <fdomingos@usp.br>
2021-03-25 10:50:51 -0300

    Experimenting w/ XML Manipulation

--- getAulaId.php
+++ getAulaId.php
@@ -8,5 +8,36 @@
     curl_close($ch);
     
     $xml = simplexml_load_string($data);
-    print_r($xml)
+    print_r($xml);
+
+    echo "<hr>";
+    echo "xml->returncode";
+    echo '<br>';
+    echo $xml->returncode;
+
+    echo "<hr>";
+    echo "xml->meetings->meeting->meetingID";
+    echo '<br>';
+    echo $xml->meetings->meeting->meetingID;
+    //->meeting->meetingID);
+    //meetingID
+    //returncodes
+
+    /*
+    SimpleXMLElement Object ( 
+        [returncode] => SUCCESS 
+        [meetings] => SimpleXMLElement Object ( 
+            [meeting] => SimpleXMLElement Object ( 
+                [meetingName] => Sala de Aula 
+                [meetingID] => 2aac419b0a458f0d3f9808be7eaaf1af1468fefc-25-16 
+                [internalMeetingID] => 5c1e0bcafdc34371b447d952f41b189497298f1f-1616679632641
+    */
+    /*
+    SimpleXMLElement Object ( 
+        [returncode] => SUCCESS 
+        [messageKey] => noMeetings 
+        [message] => no meetings were found on any servers 
+    ) 
+    echo $xml->messageKey;
+    */
 ?>
